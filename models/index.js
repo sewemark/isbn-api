05ec4d2b84a914368c38040f36fe9f74290c80b9
sewemark <sewerynjakub.markowicz@gmail.com>
2017-03-22 22:26:50 +0100

    fix

--- models/index.js
+++ models/index.js
@@ -24,7 +24,7 @@ sequelize
     });
 
 var db  = {};
-
+/*
   fs.readdirSync(__dirname)
     .filter(function(file) {
         return (file.indexOf(".") !== 0) && (file !== "index.js");
@@ -32,7 +32,9 @@ var db  = {};
     .forEach(function(file) {
         var model = sequelize.import(path.join(__dirname, file));
         db[model.name] = model;
-    });
+    });*/
+var model = sequelize.import(path.join(__dirname, 'book.js'));
+db[model.name] = model;
 
 Object.keys(db).forEach(function(modelName) {
     if ("associate" in db[modelName]) {
