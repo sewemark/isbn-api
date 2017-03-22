ce6d366524b9d3d6507cd2bc0a9e244da579b298
sewemark <sewerynjakub.markowicz@gmail.com>
2017-03-22 23:09:16 +0100

    endpoint book added

--- routes/index.js
+++ routes/index.js
@@ -17,4 +17,26 @@ router.get('/books', function(req, res) {
     });
 });
 
+router.post('/book/create', function(req, res) {
+    models.books.create({
+        ISBN: req.body.isbn,
+        Title: req.body.title,
+        Author: req.body.author,
+        PublishDate: req.body.publishDate,
+
+    }).then(function() {
+        res.redirect('/');
+    });
+});
+
+router.get('/book/:book_id/delete', function(req, res) {
+    models.books.destroy({
+        where: {
+            id: req.params.book_id
+        }
+    }).then(function() {
+        res.redirect('/');
+    });
+});
+
 module.exports = router;
