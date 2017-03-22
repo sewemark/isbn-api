051c6f72466536948815ecd009f43db48494c90c
sewemark <sewerynjakub.markowicz@gmail.com>
2017-03-22 22:19:09 +0100

    added books endpoint

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,20 @@
 var express = require('express');
 var router = express.Router();
-
+var models = require('../models');
 router.use(function(req, res, next) {
     console.log('Something is happening.');
     next();
 });
 
 router.get('/books', function(req, res) {
-    res.json({ message: 'hooray! welcome to our rest video api!' });
+    models.Books.findAll({
+        //include: [ models.Task ]
+    }).then(function(books) {
+        res.render('index', {
+            title: 'Sequelize: Bangla',
+            books: books
+        });
+    });
 });
 
 module.exports = router;
